a5203956758f921cce0451f79e380da8d787ab1b
Horia Naidin <horia.naidin@borealisgroup.com>
2023-12-12 16:51:49 +0100

    feat: meme URL

--- src/lib/bitbucket.ts
+++ src/lib/bitbucket.ts
@@ -5,7 +5,8 @@ import { Config, ReviewSumary } from './types';
 export async function postCommentToBitbucketPR(
     result: ReviewSumary,
     config: Config,
-    prId: string
+    prId: string,
+    memeUrl?: string
   ): Promise<any> {
     if (!config.bitbucket) {
       throw new Error(`Bitbucket configuration is not provided`);
@@ -16,11 +17,13 @@ export async function postCommentToBitbucketPR(
     const { accessToken, endpoint } = config.bitbucket;
     const { repoSlug, projectSlug } = config.code;
     const apiEndpoint = endpoint || `https://api.bitbucket.org/2.0/repositories/${projectSlug}/${repoSlug}/pullrequests/${prId}/comments/`;
+    const meme= `![meme](${memeUrl || 'https://cdn2.hubspot.net/hubfs/53/image8-2.jpg'  })`;
+
 
     const commentContent = result.content;
     const bodyData = {
       content: {
-        raw: commentContent,
+        raw: commentContent + ' '+meme,
       },
     };
 
